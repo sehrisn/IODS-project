6a3c7aa6d91491cff9d5643880390d90b7554ba3
Sehrish Naveed <sehrisn@uef.fi>
2019-11-11 21:45:45 +0200

    learning2014

---
+++ data/creat_learning2014.R
@@ -0,0 +1,57 @@
+# Sehrish Naveed
+
+# Date 11.11.2019
+
+# Data for open science course
+
+
+# read the data into memory
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# Look at the dimensions of the data
+dim(lrn14)
+
+# Look at the structure of the data
+str(lrn14)
+
+#Dimensions of data
+#183 obs. and 60 variables
+
+# install.packages("dplyr")
+library(dplyr)
+
+# choose a handful of columns to keep
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+# see the structure of the new dataset
+str(learning2014)
+
+# Scale all combination variables to the original scales (by taking the mean
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+# select rows where Points is greater than zero
+learning2014 <- filter(learning2014, points > 0)
+
+str(learning2014)
+
+# Save created data to folder 'data'
+write.csv(learning2014)
