619ceb240e99134c7c7b2aac6fb44cb3f5fb4270
Sehrish Naveed <sehrisn@uef.fi>
2019-11-18 00:54:44 +0200

    Data wragling

--- data/create_alc.R
+++ data/create_alc.R
@@ -81,11 +81,30 @@ for(column_name in notjoined_columns) {
 # glimpse at the new combined data
 glimpse(alc)
 
+# alc is available
+
+# access the 'tidyverse' packages dplyr and ggplot2
+library(dplyr); library(ggplot2)
+
+# define a new column alc_use by combining weekday and weekend alcohol use
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+
+# initialize a plot of alcohol use
+g1 <- ggplot(data = alc, aes(x = alc_use, fill = sex))
+
+# define the plot as a bar plot and draw it
+g1 + geom_bar()
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+
+# initialize a plot of 'high_use'
+g2 <- ggplot(alc, aes(high_use))
+
+# draw a bar plot of high_use by sex
+g2 + facet_wrap("sex") + geom_bar()
+
 #save the modified data
-write.csv(math_por)
+write.csv(alc)
 
-#Structure of modified data
-str(math_por)
 
-#Dimensions of modified data
-dim(math_por)
